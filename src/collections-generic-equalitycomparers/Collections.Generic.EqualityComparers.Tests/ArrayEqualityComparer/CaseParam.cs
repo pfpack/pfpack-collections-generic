5df8b4a160594e6cf00376d0d1e8a0bb3e9b9f56
Andrei Sergeev <a.sergeev.official@gmail.com>
2023-02-24 21:05:55 +0400

    Fix ArrayEqualityComparer/ReadOnlyListEqualityComparer case building

---
+++ src/collections-generic-equalitycomparers/Collections.Generic.EqualityComparers.Tests/ArrayEqualityComparer/CaseParam.cs
@@ -0,0 +1,8 @@
+﻿namespace PrimeFuncPack.Collections.Generic.EqualityComparers.Tests.ArrayEqualityComparer;
+
+public sealed class CaseParam<T>
+{
+    public T[] Items { get; }
+
+    public CaseParam(T[] items) => Items = items;
+}
--- src/collections-generic-equalitycomparers/Collections.Generic.EqualityComparers.Tests/ReadOnlyListEqualityComparer/EqualityComparerTestsBase_Ref.cs
+++ src/collections-generic-equalitycomparers/Collections.Generic.EqualityComparers.Tests/ReadOnlyListEqualityComparer/EqualityComparerTestsBase_Ref.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using Xunit;
 
 namespace PrimeFuncPack.Collections.Generic.EqualityComparers.Tests.ReadOnlyListEqualityComparer;
@@ -13,72 +14,92 @@ public abstract class EqualityComparerTestsBase_Ref : EqualityComparerTestsBase<
 
     [Theory]
     [MemberData(nameof(SourceAreEqualCases))]
-    public void Test_GetHashCode_SourceAreEqual_ExpectHashCodesAreEqual(string[] source1, string[] source2)
+    public void Test_GetHashCode_SourceAreEqual_ExpectHashCodesAreEqual(CaseParam<string> source1, CaseParam<string> source2)
     {
-        var hashCode1 = comparer.GetHashCode(source1);
-        var hashCode2 = comparer.GetHashCode(source2);
+        var hashCode1 = comparer.GetHashCode(source1.Items);
+        var hashCode2 = comparer.GetHashCode(source2.Items);
         Assert.StrictEqual(hashCode1, hashCode2);
     }
 
     [Theory]
     [MemberData(nameof(SourceAreEqualCases))]
-    public void Test_Equals_SourceAreEqual_ExpectTrue(string[] source1, string[] source2)
+    public void Test_Equals_SourceAreEqual_ExpectTrue(CaseParam<string> source1, CaseParam<string> source2)
     {
-        var actualEquals = comparer.Equals(source1, source2);
+        var actualEquals = comparer.Equals(source1.Items, source2.Items);
         Assert.True(actualEquals);
     }
 
     [Theory]
     [MemberData(nameof(SourceAreNotEqualCases))]
-    public void Test_Equals_SourceAreNotEqual_ExpectTrue(string[] source1, string[] source2)
+    public void Test_Equals_SourceAreNotEqual_ExpectTrue(CaseParam<string> source1, CaseParam<string> source2)
     {
-        var actualEquals = comparer.Equals(source1, source2);
+        var actualEquals = comparer.Equals(source1.Items, source2.Items);
         Assert.False(actualEquals);
     }
 
+
+
     public static IEnumerable<object[]> SourceAreEqualCases()
+        =>
+        EnumerateSourceAreEqualCases().Select(
+            @case => new object[]
+            {
+                new CaseParam<string>(@case[0]),
+                new CaseParam<string>(@case[1]),
+            });
+
+    public static IEnumerable<object[]> SourceAreNotEqualCases()
+        =>
+        EnumerateSourceAreNotEqualCases().Select(
+            @case => new object[]
+            {
+                new CaseParam<string>(@case[0]),
+                new CaseParam<string>(@case[1]),
+            });
+
+    private static IEnumerable<string[][]> EnumerateSourceAreEqualCases()
     {
-        yield return new object[]
+        yield return new string[][]
         {
             Array.Empty<string>(),
             Array.Empty<string>(),
         };
-        yield return new object[]
+        yield return new string[][]
         {
             EmptyArray<string>.Value,
             EmptyArray<string>.Value,
         };
-        yield return new object[]
+        yield return new string[][]
         {
             Array.Empty<string>(),
             EmptyArray<string>.Value,
         };
-        yield return new object[]
+        yield return new string[][]
         {
             EmptyArray<string>.Value,
             Array.Empty<string>(),
         };
-        yield return new object[]
+        yield return new string[][]
         {
             EmptyArray<string>.Create(),
             EmptyArray<string>.Create(),
         };
-        yield return new object[]
+        yield return new string[][]
         {
             new[] { "1" },
             new[] { "1" },
         };
-        yield return new object[]
+        yield return new string[][]
         {
             new[] { "1", "2" },
             new[] { "1", "2" },
         };
-        yield return new object[]
+        yield return new string[][]
         {
             new[] { "1", "2", "3" },
             new[] { "1", "2", "3" },
         };
-        yield return new object[]
+        yield return new string[][]
         {
             new[] { "1", "2", "3", "4" },
             new[] { "1", "2", "3", "4" },
@@ -88,76 +109,76 @@ public abstract class EqualityComparerTestsBase_Ref : EqualityComparerTestsBase<
         var array2 = new[] { "1", "2" };
         var array3 = new[] { "1", "2", "3" };
         var array4 = new[] { "1", "2", "3", "4" };
-        yield return new object[]
+        yield return new string[][]
         {
             array1,
             array1,
         };
-        yield return new object[]
+        yield return new string[][]
         {
             array2,
             array2,
         };
-        yield return new object[]
+        yield return new string[][]
         {
             array3,
             array3,
         };
-        yield return new object[]
+        yield return new string[][]
         {
             array4,
             array4,
         };
     }
 
-    public static IEnumerable<object[]> SourceAreNotEqualCases()
+    private static IEnumerable<string[][]> EnumerateSourceAreNotEqualCases()
     {
-        yield return new object[]
+        yield return new string[][]
         {
             Array.Empty<string>(),
             new[] { "1" },
         };
-        yield return new object[]
+        yield return new string[][]
         {
             new[] { "1" },
             Array.Empty<string>(),
         };
-        yield return new object[]
+        yield return new string[][]
         {
             EmptyArray<string>.Value,
             new[] { "1" },
         };
-        yield return new object[]
+        yield return new string[][]
         {
             new[] { "1" },
             EmptyArray<string>.Value,
         };
-        yield return new object[]
+        yield return new string[][]
         {
             new[] { "1" },
             new[] { "1", "2" },
         };
-        yield return new object[]
+        yield return new string[][]
         {
             new[] { "1", "2" },
             new[] { "1" },
         };
-        yield return new object[]
+        yield return new string[][]
         {
             new[] { "1", "2" },
             new[] { "1", "2", "3" },
         };
-        yield return new object[]
+        yield return new string[][]
         {
             new[] { "1", "2", "3" },
             new[] { "1", "2" },
         };
-        yield return new object[]
+        yield return new string[][]
         {
             new[] { "1", "2", "3" },
             new[] { "1", "2", "3", "4" },
         };
-        yield return new object[]
+        yield return new string[][]
         {
             new[] { "1", "2", "3", "4" },
             new[] { "1", "2", "3" },
