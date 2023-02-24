0e1be881f854d6a1322cd89c089a6ae87180d875
Andrei Sergeev <a.sergeev.official@gmail.com>
2023-02-24 21:23:27 +0400

    Improve CaseParam

--- src/collections-generic-equalitycomparers/Collections.Generic.EqualityComparers.Tests/ArrayEqualityComparer/CaseParam.cs
+++ src/collections-generic-equalitycomparers/Collections.Generic.EqualityComparers.Tests/ArrayEqualityComparer/CaseParam.cs
@@ -1,5 +1,7 @@
 ﻿namespace PrimeFuncPack.Collections.Generic.EqualityComparers.Tests.ArrayEqualityComparer;
 
+// This class in intended to make xUnit not to merge test cases with collections equal by value
+// (there is a difference between test cases with collections equal by value and by reference)
 public sealed class CaseParam<T>
 {
     public T[] Items { get; }
