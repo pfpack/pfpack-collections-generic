3de0508eecd525a7826b188a2895adc7de334d6c
Andrei Sergeev <a.sergeev.official@gmail.com>
2023-02-25 13:49:47 +0400

    Avoid using CaseParamBase

---
+++ src/collections-generic-equalitycomparers/Collections.Generic.EqualityComparers.Tests/CaseParam.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using System.Collections.Immutable;
+
+namespace PrimeFuncPack.Collections.Generic.EqualityComparers.Tests;
+
+// This class in intended to make xUnit not to merge test cases with collections equal by value.
+// There is a difference between the test cases with collections equal by value and by reference.
+
+public sealed record class CaseParamOfArray<T>(T[]? Items);
+
+public sealed record class CaseParamOfIReadOnlyList<T>(IReadOnlyList<T>? Items);
+
+public sealed record class CaseParamOfIList<T>(IList<T>? Items);
+
+public sealed record class CaseParamOfList<T>(List<T>? Items);
+
+public sealed record class CaseParamOfImmutableArray<T>(ImmutableArray<T> Items);
+
+public sealed record class CaseParamOfImmutableArrayNullable<T>(ImmutableArray<T>? Items);
--- src/collections-generic-equalitycomparers/Collections.Generic.EqualityComparers.Tests/CaseParamOfArray.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace PrimeFuncPack.Collections.Generic.EqualityComparers.Tests;
-
-public sealed class CaseParamOfArray<T> : CaseParamBase<T, T[]>
-{
-    public CaseParamOfArray(T[]? items) : base(items)
-    {
-    }
-}
