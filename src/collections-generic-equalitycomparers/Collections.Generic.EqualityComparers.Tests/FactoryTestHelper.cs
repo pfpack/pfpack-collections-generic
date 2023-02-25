d748ef011280b4811fbb86ca53626e7818229ca4
Andrei Sergeev <a.sergeev.official@gmail.com>
2023-02-26 00:53:01 +0400

    Add factories tests

---
+++ src/collections-generic-equalitycomparers/Collections.Generic.EqualityComparers.Tests/FactoryTestHelper.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Reflection;
+using Xunit;
+
+namespace PrimeFuncPack.Collections.Generic.EqualityComparers.Tests;
+
+internal static class FactoryTestHelper
+{
+    internal static void AssertItemComparerMatch<TCollectionComparer>(
+        Type type,
+        TCollectionComparer collectionComparer,
+        object itemComparerMatch)
+    {
+        var field = type.GetField("comparer", BindingFlags.Instance | BindingFlags.NonPublic);
+
+        Assert.NotNull(field);
+        Assert.True(field.IsPrivate);
+
+        var itemComparer = field.GetValue(collectionComparer);
+        Assert.True(ReferenceEquals(itemComparer, itemComparerMatch));
+    }
+}
---
+++ src/collections-generic-equalitycomparers/Collections.Generic.EqualityComparers.Tests/ListEqualityComparer/EqualityComparerTestsFactory.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using Xunit;
+
+namespace PrimeFuncPack.Collections.Generic.EqualityComparers.Tests.ListEqualityComparer;
+
+public sealed class EqualityComparerTestsFactory
+{
+    private static Type ComparerType => typeof(ListEqualityComparer<object>);
+
+    [Theory]
+    [MemberData(nameof(Test_Factory_ExpectItemComparer_Cases))]
+    public void Test_Factory_ExpectItemComparer(ListEqualityComparer<object> comparer, object expectedItemComparer)
+        =>
+        FactoryTestHelper.AssertItemComparerMatch(ComparerType, comparer, expectedItemComparer);
+
+    public static IEnumerable<object[]> Test_Factory_ExpectItemComparer_Cases()
+    {
+        yield return new object[]
+        {
+            ListEqualityComparer<object>.Default,
+            EqualityComparer<object>.Default
+        };
+        yield return new object[]
+        {
+            ListEqualityComparer<object>.Create(),
+            EqualityComparer<object>.Default
+        };
+        yield return new object[]
+        {
+            ListEqualityComparer<object>.Create(null),
+            EqualityComparer<object>.Default
+        };
+        yield return new object[]
+        {
+            ListEqualityComparer<object>.Create(EqualityComparer<object>.Default),
+            EqualityComparer<object>.Default
+        };
+        yield return new object[]
+        {
+            ListEqualityComparer<object>.Create(CustomEqualityComparer<object>.Default),
+            CustomEqualityComparer<object>.Default
+        };
+    }
+}
